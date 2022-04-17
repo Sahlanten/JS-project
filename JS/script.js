a661ddd05fa026d2d04ac19b482fe18f57dbfdd9
Kirill <kkipcharsky@gmail.com>
2022-04-17 19:36:33 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// console.log(1);
+// let number = 5;
+// const LeftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// const NAME_AFADF = 'Ivan'
+// const SURNAME = 'Pavlov'
+
+// const fullName = NAME + ' ' + SURnAME
+
+// const storeName = 'Vallmart';
+
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Anna', 'Greg', 'Abdul'],
+//     products: {
+//         'golova': 20,
+//         'ruka': 30
+//     },
+
+//     open: true
+// }
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + " - object");
+// console.log(4 + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;  
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(2*4 === 8);
+
+
+// const isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked || !isClose);    
